da3e27d834fe9903ab3beff1e08f0c74ad3830e8
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2017-09-03 10:37:05 +0530

    vowel.c

---
+++ vowle.c
@@ -0,0 +1,15 @@
+# priya
+#include<stdio.h>
+vod main()
+{
+int i;
+printf("enter the i values");
+scanf("%d",&i);
+{
+printf("enter the character is vowel");
+}
+else
+{
+printf("enter the character is constant");
+}
+}
