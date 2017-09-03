2e8ba97ae9c05ecfbc33b4ef6eef6c00aef70dc0
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2017-09-03 11:05:26 +0530

    sum.c

---
+++ sum.c
@@ -0,0 +1,14 @@
+# priya
+#include<stdio.h>
+void main()
+{
+int i,j,sum=0;
+printf("enter the postive number");
+scanf("%d",&i);
+for(j=0;j<i;j++);
+{
+sum+=i;
+}
+printf("sum=%d",sum);
+return 0;
+}
