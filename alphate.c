136a101e16f8c5a26af11f4a20974ce6afe262bc
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2017-09-03 10:45:41 +0530

    alphate.c

---
+++ alphate.c
@@ -0,0 +1,15 @@
+# priya
+#include<stdio.h>
+vod main()
+{
+char n;
+printf("enter a character:");
+scanf("%c",&n);
+{
+printf("enter the value alphate");
+}
+else
+{
+printf("enter the value not alphate");
+}
+}
