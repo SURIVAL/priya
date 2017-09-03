7575471b9ef00b9c1ebe89a76947c84286a0b4d1
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2017-09-03 11:51:20 +0530

    power.c

---
+++ power.c
@@ -0,0 +1,17 @@
+# priya
+#include<stdio.h>
+void main()
+{
+int base,exponent;
+printf("enter the base number:");
+scanf("%d",&base);
+printf("enter the exponent:");
+scanf("%d",&exponent);
+while(exponent!=0)
+{
+result*=base;
+--exponent;
+}
+return0;
+}
+
